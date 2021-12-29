29624d23811da077db66a2f33bb33d6ecb622611
Bill Frasure <bill.frasure@gmail.com>
2021-12-29 07:25:00 -0700

    Start framing Ecosystem data as a Zio Service

--- scalaJsExplorer/src/main/scala/org/ziverge/AppDataHardcoded.scala
+++ scalaJsExplorer/src/main/scala/org/ziverge/AppDataHardcoded.scala
@@ -4,10 +4,20 @@ import zio.ZIO
 import scalax.collection.Graph
 import scalax.collection.GraphEdge.DiEdge
 
-object AppDataHardcoded:
+trait ZioEcosystem:
+  def snapshot: ZIO[Any, Nothing, FullAppData]
+
+object ZioEcosystem:
+  def snapshot: ZIO[ZioEcosystem, Nothing, FullAppData] =
+    ZIO.serviceWithZIO(_.snapshot)
+
+object AppDataHardcoded extends ZioEcosystem:
   val getJsData =
     val connectedX = JsDataConnected.connected
     val allX       = JsData.allProjectData
     for
-      graph: Graph[Project, DiEdge] <- ZIO(ScalaGraph(allX))
+      graph: Graph[Project, DiEdge] <- ZIO.succeed(ScalaGraph(allX))
     yield FullAppData(connectedX, allX, graph)
+
+  val snapshot: ZIO[Any, Nothing, FullAppData] =
+    getJsData
