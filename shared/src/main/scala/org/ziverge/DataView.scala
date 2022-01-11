241dc0ca8aad5468bb37cbb99a239d47ec218fb0
Bill Frasure <bill.frasure@gmail.com>
2022-01-11 16:22:49 -0700

    More deletions and renames

---
+++ shared/src/main/scala/org/ziverge/DataView.scala
@@ -0,0 +1,25 @@
+package org.ziverge
+
+import ujson.Js
+import zio.Console.printLine
+import zio.{Chunk, ZIO}
+import upickle.default.{macroRW, ReadWriter as RW, *}
+import java.io.ObjectInputStream
+import java.io.ByteArrayInputStream
+
+enum DataView:
+  case Dependencies, Dependents, Blockers
+
+object DataView:
+
+  def fromStrings(
+      args: Chunk[String]
+  ): Option[DataView] = // TODO Decide whether to do with multiple args
+    args.flatMap(fromString).headOption
+
+  def fromString(args: String): Option[DataView] =
+    values.find(_.toString == args)
+
+  import upickle.default.ReadWriter.join
+
+  implicit val explorerRW: RW[DataView] = macroRW
