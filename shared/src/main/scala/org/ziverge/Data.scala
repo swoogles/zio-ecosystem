e05f5103a99d25e0ef5d0138919138c3c79dc8b8
Bill Frasure <bill.frasure@gmail.com>
2022-01-17 16:07:30 -0700

    +project Github links

--- shared/src/main/scala/org/ziverge/Data.scala
+++ shared/src/main/scala/org/ziverge/Data.scala
@@ -7,14 +7,14 @@ object Data:
   val coreProjects =
     List(
       zioCore,
-      Project("dev.zio", "zio-test"),
-      Project("dev.zio", "zio-test-sbt"),
-      Project("dev.zio", "zio-test-magnolia"),
-      Project("dev.zio", "zio-internal-macros"),
-      Project("dev.zio", "zio-stacktracer"),
-      Project("dev.zio", "izumi-reflect"),
-      Project("dev.zio", "zio-streams"),
-      Project("dev.zio", "izumi-reflect-thirdparty-boopickle-shaded")
+      Project("dev.zio", "zio-test", Some("https://github.com/zio/zio")),
+      Project("dev.zio", "zio-test-sbt", Some("https://github.com/zio/zio")),
+      Project("dev.zio", "zio-test-magnolia", Some("https://github.com/zio/zio")),
+      Project("dev.zio", "zio-internal-macros", Some("https://github.com/zio/zio")),
+      Project("dev.zio", "zio-stacktracer", Some("https://github.com/zio/zio")),
+      Project("dev.zio", "zio-streams", Some("https://github.com/zio/zio")),
+      Project("dev.zio", "izumi-reflect", Some("https://github.com/zio/izumi-reflect")),
+      Project("dev.zio", "izumi-reflect-thirdparty-boopickle-shaded", Some("https://github.com/zio/izumi-reflect"))
     )
 
   val projects =
@@ -28,7 +28,7 @@ object Data:
         Project("dev.zio", "zio-query", Some("https://github.com/zio/zio-query")),
         Project("dev.zio", "zio-schema", Some("https://github.com/zio/zio-schema")),
         Project("dev.zio", "zio-config", Some("https://github.com/zio/zio-config")),
-        Project("dev.zio", "zio-config-typesafe"), // , Some("https://github.com/zio/zio-config")),
+        Project("dev.zio", "zio-config-typesafe", Some("https://github.com/zio/zio-config")),
         Project("dev.zio", "zio-kafka", Some("https://github.com/zio/zio-kafka")),
         Project("dev.zio", "zio-ftp", Some("https://github.com/zio/zio-ftp")),
         Project("dev.zio", "zio-aws-core", Some("https://github.com/zio/zio-aws")),
@@ -59,7 +59,7 @@ object Data:
           Some("https://github.com/softwaremill/sttp")
         ),
         Project("com.softwaremill.sttp.shared", "zio"),
-        Project("io.d11", "zhttp"), // , Some("https://github.com/dream11/zio-http")),
+        Project("io.d11", "zhttp", Some("https://github.com/dream11/zio-http")),
         Project("dev.zio", "zio-interop-cats", Some("https://github.com/zio/interop-cats")),
         Project("dev.zio", "zio-nio", Some("https://github.com/zio/zio-nio")),
         Project("dev.zio", "zio-zmx", Some("https://github.com/zio/zio-zmx")),
