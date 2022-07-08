559cd511a28abc6273328838cd653d6d072fb856
Bill Frasure <bill.frasure@gmail.com>
2022-07-08 15:43:22 -0600

    Revert "Revert "Fix Server #time 20m""
    This reverts commit 047461a11acd8f5336ed47f87619b1631095b645.

--- src/main/scala/org/ziverge/Server.scala
+++ src/main/scala/org/ziverge/Server.scala
@@ -13,46 +13,59 @@ import java.nio.file.Paths
 import java.io.File
 import java.time.Instant
 
-object DependencyServer extends App:
+
+object DependencyServer extends ZIOAppDefault:
 
   import upickle.default.{read, write}
-  val app =
-    Http.collectHttp[Request] {
-      case Method.GET -> Root =>
-        Http.fromStream {
-          ZStream.fromFile(Paths.get("src/main/resources/index.html").toFile).refineOrDie(_ => ???)
-        }
-      case Method.GET -> Root / "compiledJavascript" / "zioecosystemtracker-fastopt.js" =>
-        Http.fromStream {
-          ZStream
-            .fromFile(
-              Paths
-                .get("src/main/resources/compiledJavascript/zioecosystemtracker-fastopt.js")
-                .toFile
-            )
-            .refineOrDie(_ => ???)
-        }
-      case Method.GET -> Root / "images" / path =>
-        Http
-          .fromFile(new File(s"src/main/resources/images/$path"))
-          .setHeaders(Headers.contentType("image/svg+xml"))
+  val app: HttpApp[Any, Nothing] = Http.collect[Request] {
+    case Method.GET -> !! / "text" => Response.text("Hello World!")
+    case Method.GET -> !! / "json" => Response.json("""{"greetings": "Hello World!"}""")
+  }
 
-      case Method.GET -> !! / "projectData" =>
+  val app2: HttpApp[Any, Throwable] =
+    Http.collect[Request] {
+      case Method.GET -> !! =>
+//        Response(Status.Ok, Headers.empty,
+//          HttpData.fromStream {
+//          ZStream.fromFile(Paths.get("src/main/resources/index.html").toFile).refineOrDie(_ => ???)
+//        }
+//      )
+        Response.text("hey")
+      case Method.GET -> zhttp.http.!! / "compiledJavascript" / "zioecosystemtracker-fastopt.js" =>
+        Response(Status.Ok, Headers.empty,
+          HttpData.fromStream {
+            ZStream
+              .fromFile(
+                Paths
+                  .get("src/main/resources/compiledJavascript/zioecosystemtracker-fastopt.js")
+                  .toFile
+              )
+              .refineOrDie(_ => ???)
+          }
+        )
+      case Method.GET -> zhttp.http.!! / "images" / path =>
+        Response(
+          status = Status.Ok,
+          headers =
+            Headers.contentType("image/svg+xml"),
+          HttpData
+          .fromFile(new File(s"src/main/resources/images/$path"))
+        )
+
+      case Method.GET -> zhttp.http.!! / "projectData" =>
         val appData      = CrappySideEffectingCache.fullAppData.get
         val responseText = Chunk.fromArray(write(appData).getBytes)
-        Http.response(
-          Response.http(
-            status = Status.OK,
+          Response(
+            status = Status.Ok,
             headers =
               Headers
                 .contentLength(responseText.length.toLong)
                 .combine(Headers.contentType("application/json")),
             data = HttpData.fromStream(ZStream.fromChunk(responseText))
           )
-        )
     }
 
-  override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] =
+  override def run =
     (
       for
         port <- ZIO.succeed(sys.env.get("PORT"))
@@ -63,8 +76,6 @@ object DependencyServer extends App:
             .orDie
             .repeat(Schedule.spaced(30.minutes))
             .fork
-        _ <- Server.start(port.map(_.toInt).getOrElse(8090), app)
+        _ <- Server.start(port.map(_.toInt).getOrElse(8090), app).exitCode
       yield ()).exitCode
 end DependencyServer
-
-
--- src/main/scala/org/ziverge/SharedLogic.scala
+++ src/main/scala/org/ziverge/SharedLogic.scala
@@ -9,66 +9,58 @@ import java.time.Instant
 object CrappySideEffectingCache:
   var fullAppData: Option[FullAppData] = None
   var timestamp: Instant               = Instant.parse("2018-11-30T18:35:24.00Z")
-  
+
 object SharedLogic:
   def fetchAppDataAndRefreshCache(scalaVersion: ScalaVersion) =
     for
       _   <- zio.Console.printLine("Getting fresh data")
-      res <- fetchAppData(scalaVersion).debug("Full Data dump")
+      res <- fetchAppData(scalaVersion)
       _ <-
-        ZIO {
+        ZIO.attempt {
           CrappySideEffectingCache.fullAppData = Some(res)
         }
     yield res
 
   def fetchAppData(scalaVersion: ScalaVersion): ZIO[Any, Throwable, FullAppData] =
     for
-      currentZioVersion: Version <-
+      currentZioVersion: org.ziverge.Version <-
         Maven.projectMetaDataFor(TrackedProjects.zioCore, scalaVersion).map(_.typedVersion)
       allProjectsMetaData <-
-        ZIO.collectAllSuccessesPar(
-          TrackedProjects
-            .projects
-            .map { project =>
-              Maven.projectMetaDataFor(project, scalaVersion)
-            }
+        ZIO.collectAllSuccessesPar(TrackedProjects.projects.map{ project =>
+          Maven.projectMetaDataFor(project, scalaVersion)
+        }
         )
       // TODO Do Pull Request query here
-      graph: Graph[Project, DiEdge] <- ZIO(ScalaGraph(allProjectsMetaData))
+      graph: Graph[Project, DiEdge] <- ZIO.attempt(ScalaGraph(allProjectsMetaData))
       connectedProjects: Seq[ConnectedProjectData] <-
-        ZIO.collectAllSuccessesPar(
-          allProjectsMetaData.map(x =>
-            for
-              res <-
-                ZIO
-                  .fromEither(
-                    ConnectedProjectData(x, allProjectsMetaData, graph, currentZioVersion)
+        ZIO.collectAllSuccessesPar(allProjectsMetaData.map(x =>
+          for
+            res <-
+              ZIO.fromEither(ConnectedProjectData(x, allProjectsMetaData, graph, currentZioVersion))
+                .tapError(error => ZIO.debug("Error constructing ConnectedProjectData: " + error))
+            finalProject <-
+              if (res.onLatestZioDep)
+                ZIO.succeed(res)
+              else
+                res
+                  .project
+                  .githubOrgAndRepo
+                  .map(project =>
+                    Github
+                      .pullRequests(project)
+                      .map { prOpt =>
+                        res.copy(relevantPr = prOpt)
+                      }
+                      .catchAll { case githubError =>
+                        println("Github Error: " + githubError)
+                        ZIO.succeed(res)
+                      }
                   )
-                  .tapError(error => ZIO.debug("Error constructing ConnectedProjectData: " + error))
-              finalProject <-
-                if (res.onLatestZioDep)
-                  ZIO.succeed(res)
-                else
-                  res
-                    .project
-                    .githubOrgAndRepo
-                    .map(project =>
-                      Github
-                        .pullRequests(project)
-                        .map { prOpt =>
-                          res.copy(relevantPr = prOpt)
-                        }
-                        .catchAll { case githubError =>
-                          println("Github Error: " + githubError)
-                          ZIO.succeed(res)
-                        }
-                    )
-                    .getOrElse(ZIO.succeed(res))
-
-            // ZIO.debug(res.project.artifactId + " upToDate: " +  res.projectIsUpToDate)
-            // TODO Look for PRs here
-            yield finalProject
-          )
+                  .getOrElse(ZIO.succeed(res))
+          // ZIO.debug(res.project.artifactId + " upToDate: " +  res.projectIsUpToDate)
+          // TODO Look for PRs here
+          yield finalProject
+        )
         )
       res =
         FullAppData(
